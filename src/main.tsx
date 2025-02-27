e947deff0fbaf9b1799c8f6fd2564596748a6514
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-02-27 07:16:33 -0500

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react"
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
@@ -11,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Provider>
         <App />
+        <Analytics />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
