40fbc58b2433ca3933c36adcef293e76e3df9402
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-06 16:49:19 -0500

    add seo in shop page

--- src/lib/constants/seoPages.ts
+++ src/lib/constants/seoPages.ts
@@ -1,11 +1,34 @@
 import { SEOFields } from "../types/models/seoFields";
 
-export const indexPage: SEOFields = 
-  {
-    title: "TrinUp: Nutrición y superación",
-    description: "TrinUp es una app donde toda persona merece o puede mejorar su estilo de vida con buenos hábitos y nutrición.",
-    keywords:["nutrición", "salud", "bienestar", "hábitos saludables", "TrinUp", "superación personal"],
-    url: "https://trinup-app.vercel.app",
-    image: "https://trinup-app.vercel.app/images/logo.webp",
-  }
+export const indexPage: SEOFields = {
+  title: "TrinUp: Nutrición y superación",
+  description:
+    "TrinUp es una app donde toda persona merece o puede mejorar su estilo de vida con buenos hábitos y nutrición.",
+  keywords: [
+    "nutrición",
+    "salud",
+    "bienestar",
+    "hábitos saludables",
+    "TrinUp",
+    "superación personal",
+  ],
+  url: "https://trinup-app.vercel.app",
+  image: "https://trinup-app.vercel.app/images/logo.webp",
+};
 
+export const shopPage: SEOFields = {
+  title: "Tienda TrinUp",
+  description:
+    "Descubre en la tienda de TrinUp productos seleccionados para mejorar tu salud, nutrición y estilo de vida. ¡Haz del bienestar tu prioridad!",
+  keywords: [
+    "productos saludables",
+    "bienestar",
+    "nutrición",
+    "TrinUp",
+    "salud y bienestar",
+    "hábitos saludables",
+    "compras saludables",
+  ],
+  url: "https://trinup-app.vercel.app/tienda",
+  image: "https://trinup-app.vercel.app/images/mall.webp",
+};
--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,8 +1,30 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/components/templates/default";
+import { Helmet } from "react-helmet-async";
+import { shopPage } from "@/lib/constants/seoPages"
 
 export default function ShopPage() {
   return (
+    <>
+    <Helmet>
+      {/* Primary Meta Tags */}
+      <title>{shopPage.title}</title>
+      <meta name="title" content={shopPage.title} />
+      <meta name="description" content={shopPage.description} />
+      <meta name="keywords" content={shopPage.keywords.join(", ")} />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:url" content={shopPage.url} />
+      <meta property="og:title" content={shopPage.title} />
+      <meta property="og:description" content={shopPage.description} />
+      <meta property="og:image" content={shopPage.image} />
+
+      {/* Twitter */}
+      <meta name="twitter:url" content={shopPage.url} />
+      <meta name="twitter:title" content={shopPage.title} />
+      <meta name="twitter:description" content={shopPage.description} />
+      <meta name="twitter:image" content={shopPage.image} />
+    </Helmet>
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
@@ -10,5 +32,6 @@ export default function ShopPage() {
         </div>
       </section>
     </DefaultLayout>
+    </>
   );
 }
