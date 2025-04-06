3a5c5cadc69c8416885b9b482040f0c8141704aa
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-06 16:18:39 -0500

    add seo dynamic in home page

---
+++ src/lib/constants/seoPages.ts
@@ -0,0 +1,11 @@
+import { SEOFields } from "../types/models/seoFields";
+
+export const indexPage: SEOFields = 
+  {
+    title: "TrinUp: Nutrición y superación",
+    description: "TrinUp es una app donde toda persona merece o puede mejorar su estilo de vida con buenos hábitos y nutrición.",
+    keywords:["nutrición", "salud", "bienestar", "hábitos saludables", "TrinUp", "superación personal"],
+    url: "https://trinup-app.vercel.app",
+    image: "https://trinup-app.vercel.app/images/logo.webp",
+  }
+
