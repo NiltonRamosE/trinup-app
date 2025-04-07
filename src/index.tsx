94648e22495cfd93d7c099c78cb12133c6fd791e
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-04-06 19:16:28 -0500

    Revert "test de seo eliminando el meta en el index para cargar los del ts"
    This reverts commit 4d7c7815c9feb6f74e23e0a4007ca28c4871eff5.

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,14 @@ import { Analytics } from "@vercel/analytics/react"
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
-import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <Provider>
-          <App />
-          <Analytics />
-        </Provider>
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+        <Analytics />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
